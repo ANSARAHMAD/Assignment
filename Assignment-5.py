26da003cc05fb638dab76ba3596230f3d096d53f
ZeHaR <anas14529@gmail.com>
2019-10-04 23:33:40 +0500

    Assignment Commit

---
+++ Assignment-5.py
@@ -0,0 +1,22 @@
+#Conversing with python
+
+print("Hello World")
+
+print("You must be the legendary god that comes from the sky")
+
+print("We have been waiting for you for a long time")
+
+print('Our legend says you will be very tasty with mustard')
+
+x=43
+x = x+1
+print(x)
+
+
+# Errors which May Occur
+
+primt ("Hello World")
+
+print "Hello World"
+
+I hate you python
